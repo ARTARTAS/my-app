6113c4ea25c3e405b282094e517c97d06f85c6e1
ARTARTAS <jorjio21@gmail.com>
2021-10-05 11:56:53 +0300

    Update Users.jsx
    Переход на классовую компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,59 +2,70 @@ import * as axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {        
-        props.setUsers(response.data.items);
+      .then((response) => {
+        this.props.setUsers(response.data.items);
       });
   }
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id} className={s.userArea}>
-          <div>
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id} className={s.userArea}>
             <div>
-              <img className={s.avatar} src={u.photos.large !=null ? u.photos.large : "https://html5css.ru/howto/img_avatar.png" }/>
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  className={s.followButton}
-                  onClick={() => {
-                    props.unfollowUser(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  className={s.unfollowButton}
-                  onClick={() => {
-                    props.followUser(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </div>
-          <div className={s.description}>
-            <div className={s.leftBlock}>
-              <div className={s.fullName}>{u.name}</div>
-              <div className={s.status}>
-                <div>{u.status}</div>
+              <div>
+                <img
+                  className={s.avatar}
+                  src={
+                    u.photos.large != null
+                      ? u.photos.large
+                      : "https://html5css.ru/howto/img_avatar.png"
+                  }
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    className={s.followButton}
+                    onClick={() => {
+                      this.props.unfollowUser(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    className={s.unfollowButton}
+                    onClick={() => {
+                      this.props.followUser(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
               </div>
             </div>
-            <div className={s.rightBlock}>
-              <div>{"u.location.city"}</div> 
-              <div>{"u.location.country"}</div>
+            <div className={s.description}>
+              <div className={s.leftBlock}>
+                <div className={s.fullName}>{u.name}</div>
+                <div className={s.status}>
+                  <div>{u.status}</div>
+                </div>
+              </div>
+              <div className={s.rightBlock}>
+                <div>{"u.location.city"}</div>
+                <div>{"u.location.country"}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Users;
