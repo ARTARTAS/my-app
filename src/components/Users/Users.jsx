e62e9b473050d585bc34f1197876a98b9518d0bc
ARTARTAS <jorjio21@gmail.com>
2021-10-05 10:53:16 +0300

    Edit
    Получение users GET запросом на сервер.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,22 @@
+import * as axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
 
 const Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {        
+        props.setUsers(response.data.items);
+      });
+  }
   return (
     <div>
       {props.users.map((u) => (
         <div key={u.id} className={s.userArea}>
           <div>
             <div>
-              <img className={s.avatar} src={u.photoURL} alt="avatar" />
+              <img className={s.avatar} src={u.photos.large !=null ? u.photos.large : "https://html5css.ru/howto/img_avatar.png" }/>
             </div>
             <div>
               {u.followed ? (
@@ -34,14 +42,14 @@ const Users = (props) => {
           </div>
           <div className={s.description}>
             <div className={s.leftBlock}>
-              <div className={s.fullName}>{u.fullName}</div>
+              <div className={s.fullName}>{u.name}</div>
               <div className={s.status}>
                 <div>{u.status}</div>
               </div>
             </div>
             <div className={s.rightBlock}>
-              <div>{u.location.city}</div>
-              <div>{u.location.country}</div>
+              <div>{"u.location.city"}</div> 
+              <div>{"u.location.country"}</div>
             </div>
           </div>
         </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,52 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 let initialState = {
-    users: [
-        {
-            id: 1,
-            photoURL: "https://html5css.ru/howto/img_avatar.png",
-            followed: true,
-            fullName: 'Dmitriy',
-            status: 'User status 1',
-            location: {
-                city: 'Kyiv',
-                country: 'Ukraine'
-            }
-        },
-        {
-            id: 2,
-            photoURL: "https://html5css.ru/howto/img_avatar.png",
-            followed: false,
-            fullName: 'Petya',
-            status: 'User status 2',
-            location: {
-                city: 'Moscow',
-                country: 'Russia'
-            }
-        },
-        {
-            id: 3,
-            photoURL: "https://html5css.ru/howto/img_avatar.png",
-            followed: true,
-            fullName: 'Vasya',
-            status: 'User status 3',
-            location: {
-                city: 'Odessa',
-                country: 'Ukraine'
-            }
-        },
-        {
-            id: 4,
-            photoURL: "https://html5css.ru/howto/img_avatar.png",
-            followed: true,
-            fullName: 'Viktor',
-            status: 'User status 4',
-            location: {
-                city: 'Texas',
-                country: 'USA'
-            }
-        }
-    ]
+    users: []
 }
 
 const usersReducer = (state = initialState, action) => {
