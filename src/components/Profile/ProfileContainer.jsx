12b045a5c1bce089fa69ce4b5ea76c99cc823d4f
ARTARTAS <jorjio21@gmail.com>
2021-10-07 10:47:40 +0300

    Edit
    использование withRouter для получения данных из URL для Profile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,17 +3,22 @@ import React from "react";
 import { connect } from "react-redux";
 import Profile from "./Profile";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { withRouter } from "react-router";
 
 class ProfileAPI extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if(!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
   }
   render() {
-    return <Profile {...this.props} profile={this.props.profile}/>;
+    return <Profile {...this.props} profile={this.props.profile} />;
   }
 }
 
@@ -21,8 +26,10 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
+let WithUrlDataContainerComponent = withRouter(ProfileAPI);
+
 const ProfileContainer = connect(mapStateToProps, { setUserProfile })(
-  ProfileAPI
+  WithUrlDataContainerComponent
 );
 
 export default ProfileContainer;
