dfc31eb21af0cee0aba78ee7eae0b217810e40f1
ARTARTAS <jorjio21@gmail.com>
2021-10-12 15:18:16 +0300

    Edit
    Создание формы с помощью redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,46 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+import s from "./Login.module.css";
+
+export const LoginForm = (props) => {
+  return (
+    <form className={s.login__form} onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          className={s.login}
+          component={"input"}
+          type="email"
+          name="login"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <Field
+          className={s.password}
+          component={"input"}
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
+      <div>
+        <Field component={"input"} type="checkbox" name="rememberMe" />
+        Remember me
+      </div>
+      <button className={s.button}>Login</button>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  form: "Login",
+})(LoginForm);
 
 const Login = (props) => {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="email" />
-        </div>
-        <div>
-          <input type="password"/>{" "}
-        </div>
-        <div>
-            <input type="checkbox" />
-        </div>
-        <input type="button" value="" />
-      </form>
+    <div className={s.login__wrapper}>
+      <h1 className={s.login__title}>Login</h1>
+      <LoginReduxForm  />
     </div>
   );
 };
