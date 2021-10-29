e41f668ac48b6587f877760b667839581cb9cf22
ARTARTAS <jorjio21@gmail.com>
2021-10-29 12:45:22 +0300

    Edit
    refactoring

--- src/API/auth-api.ts
+++ src/API/auth-api.ts
@@ -1,25 +1,22 @@
 import { instance, ResultCodeEnum, ResultCodeForCaptchaEnum } from './API';
-type getAuthMeResponseType = {
-    data: { id: number, email: string, login: string }
-    resultCode: ResultCodeEnum
+
+type ResponseType<D = {}, RC = ResultCodeEnum> = {
+    data: D
     messages: Array<string>
+    resultCode: RC
 }
-type LoginResponseType = {
-    data: {
-        data: {
-            userId: number
-        }
-        resultCode: ResultCodeEnum | ResultCodeForCaptchaEnum
-        messages: Array<string>
-    }
+
+type getAuthMeDataResponseType = {
+    id: number,
+    email: string,
+    login: string
 
 }
-type LogoutResponseType = {
-    data: {}
-    resultCode: ResultCodeEnum
-    messages: Array<string>
-    fieldsErrors: Array<string>
+
+type LoginDataResponseType = {
+    userId: number
 }
+
 type LoginMeDataType = {
     email: string
     password: string
@@ -29,15 +26,15 @@ type LoginMeDataType = {
 export const authAPI = {
     getAuthMe() {
         return (
-            instance.get<getAuthMeResponseType>(`auth/me`)
+            instance.get<ResponseType<getAuthMeDataResponseType>>(`auth/me`)
         ).then(response => response.data);
     },
     login(email: string, password: string, rememberMe: boolean = false, captcha: string | null = null) {
-        return instance.post<LoginMeDataType, LoginResponseType>(`auth/login`, { email, password, rememberMe, captcha }).then(response => response.data)
+        return instance.post<LoginMeDataType, ResponseType<LoginDataResponseType, ResultCodeEnum | ResultCodeForCaptchaEnum>>(`auth/login`, { email, password, rememberMe, captcha })
     },
     logout() {
         return (
-            instance.delete<LogoutResponseType>(`auth/login`)
+            instance.delete<ResponseType>(`auth/login`)
         ).then(response => response.data);
     }
 }
--- src/API/user-api.ts
+++ src/API/user-api.ts
@@ -1,18 +1,8 @@
+import { UsersType } from './../redux/users-reducer';
 import { instance } from './API';
 
 type getUsersResponseType = {
-    items: [
-        {
-            name: string,
-            id: number,
-            photos: {
-                small: string | null,
-                large: string | null
-            },
-            status: string | null,
-            followed: boolean
-        }
-    ],
+    items: Array<UsersType>,
     totalCount: number,
     error: string | null
 }
